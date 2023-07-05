d820937c8ceda103c34d64746eae9cb8f9a3aedf
alex-bundi <bundialex45@gmail.com>
2023-07-05 14:08:08 +0300

    New Fuctionality

--- main.py
+++ main.py
@@ -7,27 +7,31 @@ class ScanDocument:
     """
     read_contents = ReadContents()
     
-    def __init__(self):
+    def __init__(self) -> None:
         self.__all_file_names = []
         self.store_file_names() # Populates file names only once.
 
     
     @classmethod
-    def get_docs(cls)-> Path:
+    def get_docs(cls):
         """Prompts the user for a directory containing only the files to scan/read."""
 
         cls.dir_path = input("Directory: ").strip()
+        cls.attempts = 0
+        cls.max_attempts = 5
 
         while True:
-            cls.src_dir = Path(cls.dir_path)
-            if cls.src_dir.is_dir(): # If provided path exists in the OS.
-                return cls.src_dir
-            else:
-                cls.dir_path = input("Invalid directory. Please enter a valid directory: ").strip()
+            if len(cls.dir_path) != 0:
+                cls.src_dir = Path(cls.dir_path)
+                if cls.src_dir.is_dir():  # If provided path exists in the OS.
+                    return cls.src_dir
+            elif cls.attempts == cls.max_attempts:
+                raise ValueError("Exceeded maximum number of attempts. Unable to find a valid directory or input is invalid.")
 
-        
+            cls.dir_path = input("Invalid directory. Please enter a valid directory: ").strip()
+            cls.attempts += 1     
 
-    def get_files(self):
+    def get_files(self) -> None:
         """Retrieves exclusively the files located in the returned directory by the getdoc() function."""
         self.get_docs()
 
@@ -40,26 +44,35 @@ class ScanDocument:
             self.ext[file_extension] = self.ext.get(file_extension, 0) + 1 # File extensions.
 
         print("\n")
-        print(f"Extensions:\n{self.ext}")
+        print(f"Extensions:\n{self.ext}\n")
 
     @property
     def all_file_names(self): # To avoid modification of the list.
         return self.__all_file_names
     
-    def store_file_names(self):
+    def store_file_names(self) -> None:
+        """Stores all file names in specified directory."""
+
         self.get_files()
 
         for file_name, file_extension in self.present_files:
             self.__all_file_names.append(file_name)
 
+    def file_type_filter(self, ftype):
+        """"Filters functionality for files in specified directory according to thier extension."""
+        self.ftype = ftype
+        self.filters = filter(lambda file_name: file_name.endswith(self.ftype), self.all_file_names)
+        return self.filters
+
     def choose_file_type(self):
         
         self.all_file_names
         self.pick_type = input("Which file type(s) do you want to scan? ").strip().lower()
-
+        
         if self.pick_type == ".txt": # Read .txt files
             self.txt_files = filter(lambda file_name: file_name.endswith('.txt'), self.all_file_names)
-            print(list(self.txt_files)) # Counter the filter object returned 0by txt_files
+            self.file_type_filter(ftype= ".txt")
+            print(f"{list(self.txt_files)}\n") # Counter the filter object returned by txt_files
 
             self.read_contents.read_txt(files_src= self.src_dir, files_present= self.present_files)
 
--- read_files/read_file_contents.py
+++ read_files/read_file_contents.py
@@ -7,10 +7,14 @@ class ReadContents:
         """Only reads documents with the file extension .txt"""
         self.files_src = files_src
         self.files_present = files_present
+        
 
         for file_name, file_ext in self.files_present:
             if file_ext == ".txt":
+                self.txt_info = {}
                 self.full_path = pathlib.PurePath(self.files_src, file_name) # To avoid FileNotFoundError
                 with open(self.full_path, "r") as scanned_file:
-                    self.data = scanned_file.read()
-                    #print(self.data)
+                    self.content = scanned_file.read()
+                    self.txt_info["filename"] = file_name
+                    self.txt_info["content"] = self.content.replace("\n", "").lower()
+                print(self.txt_info)
